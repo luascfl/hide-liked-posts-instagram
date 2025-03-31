1d5cd3ad25728d80b9f2d2732d49b2e5c735d075
Lucas Camilo Carvalho <109776371+luascfl@users.noreply.github.com>
2025-03-30 21:38:16 -0300

    Update hide-liked-posts-instagram.user.js

--- hide-liked-posts-instagram.user.js
+++ hide-liked-posts-instagram.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         Hide Liked Posts on Instagram (With Periodic Scroll Fix)
 // @namespace    http://tampermonkey.net/
-// @version      1.7
+// @version      1.8
 // @description  Hides posts you've liked on instagram.com, scrolling automatically every 2 seconds to ensure the page loads correctly without black screens.
 // @author       luascfl
 // @match        https://www.instagram.com/*
 // @updateURL    https://raw.githubusercontent.com/luascfl/hide-liked-posts-instagram/main/hide-liked-posts-instagram.user.js
 // @downloadURL  https://raw.githubusercontent.com/luascfl/hide-liked-posts-instagram/main/hide-liked-posts-instagram.user.js
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 (function() {
